1312018b01410535076ab6905967e9dcacfe64bc
Ajay Chani <ajay.chani@gmail.com>
2025-03-28 00:48:04 +0100

    Update add_date_cols.py

--- add_date_cols.py
+++ add_date_cols.py
@@ -23,3 +23,43 @@ for date_str in end_of_quarters:
     """
     df = df.withColumn(col_name, expr(condition_expr))
 
+
+
+from pyspark.sql.functions import col, lit, when, struct, array, explode
+
+# List of end-of-quarter dates
+end_of_quarters = [
+    '2025-03-31',
+    '2025-06-30',
+    '2025-09-30',
+    '2025-12-31',
+    '2026-03-31',
+    '2026-06-30',
+    '2026-09-30',
+    '2026-12-31'
+]
+
+# Create array of structs: [{date, total_size_if_required_by_false}, ...]
+exploded_array = array(*[
+    struct(
+        lit(date_str).alias("date"),
+        when(
+            col(f"required_by_{date_str.replace('-', '_')}") == False,
+            col("size")
+        ).otherwise(lit(0)).alias("size_to_sum")
+    )
+    for date_str in end_of_quarters
+])
+
+# Add array column and explode it
+df_with_array = df.withColumn("exploded_structs", exploded_array)
+df_exploded = df_with_array.select(explode("exploded_structs").alias("row"))
+
+# Select and group by date
+df_final = df_exploded.selectExpr("cast(row.date as date) as date", "row.size_to_sum as size") \
+                      .groupBy("date") \
+                      .sum("size") \
+                      .withColumnRenamed("sum(size)", "total_size") \
+                      .orderBy("date")
+
+df_final.show()
